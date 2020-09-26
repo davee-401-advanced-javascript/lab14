0cf3ccb718a87c263fa1d07e50e09592eb4dc0f5
Davee Sok <daveesok@gmail.com>
2020-09-26 00:37:29 -0700

    moved required env file

--- __tests__/auth-routes.test.js
+++ __tests__/auth-routes.test.js
@@ -3,8 +3,8 @@
 const supergoose = require('@code-fellows/supergoose');
 const server = require('../server.js');
 const myServer = supergoose(server.app);
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 describe('Proof of Life test', () => {
   it('Proof of life', () => {
