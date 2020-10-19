60da25a0021b5c773bcc0e7a3804857e665d4466
Davee Sok <daveesok@gmail.com>
2020-10-19 14:25:27 -0700

    updated

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const cors = require('cors');
 
 const notFoundHandler = require('./middleware/404.js');
 const errorHandler = require('./middleware/500.js');
+const timeStamp = require('./middleware/timestamp.js');
+const logger = require('./middleware/logger.js');
 const authRoutes = require('./auth/routes/auth-router.js');
 const v1Routes = require('./api/v1.js');
 const v2Routes = require('./api/v2.js');
 
 const app = express();
 
+app.use(timeStamp);
+app.use(logger);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
