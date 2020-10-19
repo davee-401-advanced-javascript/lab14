65bad5eff043130c97c2b2c7a6e1dcf0dcf35f65
Davee Sok <daveesok@gmail.com>
2020-10-19 13:40:02 -0700

    updated

--- auth/routes/auth-router.js
+++ auth/routes/auth-router.js
@@ -8,19 +8,26 @@ const bearerAuth = require('../middleware/bearer.js');
 const can = require('../middleware/acl.js');
 const userModel = require('../models/users-model.js');
 
-router.post('/signup', async (req, res, next) => {
+router.post('/signup', handleSignUp);
+router.post('/signin', basicAuth, handleSignIn);
+router.get('/secret', bearerAuth, handleSecretRoute);
+router.get('/article', bearerAuth, can('read'), userCanRead);
+router.get('/article', bearerAuth, can('create'), userCanCreate);
+router.get('/article', bearerAuth, can('update'), userCanUpdate);
 
+
+async function handleSignUp(req, res, next) {
   try {
     let obj = {
       username: req.body.username,
       password: req.body.password,
       role: req.body.role,
     };
-
+    
     let record = new userModel(obj);
     let newUser = await record.save();
     let token = record.generateToken();
-
+    
     let output = {
       token: token,
       user: newUser,
@@ -30,10 +37,9 @@ router.post('/signup', async (req, res, next) => {
   } catch (e) {
     next(e.message);
   }
-});
+}
 
-
-router.post('/signin', basicAuth, (req, res, next) => {
+async function handleSignIn(req, res, next) {
   try {
     let object = {
       token: req.token,
@@ -44,22 +50,23 @@ router.post('/signin', basicAuth, (req, res, next) => {
   } catch(e) {
     next(e.message);
   }
-});
+}
 
-router.get('/secret', bearerAuth, (req, res) => {
+function handleSecretRoute(req, res, next) {
   res.status(200).send(`Welcome, ${req.user.username}`);
-});
+}
 
-router.get('/article', bearerAuth, can('read'), (req, res) => {
+function userCanRead(req, res, next) {
   res.status(200).send('You can read it');
-});
+}
 
-router.post('/article', bearerAuth, can('create'), (req, res) => {
+function userCanCreate(req, res, next) {
   res.status(200).send('You can create it');
-});
+}
 
-router.put('/article', bearerAuth, can('update'), (req, res) => {
+function userCanUpdate(req, res, next) {
   res.status(200).send('You can update it');
-});
+}
+
 
 module.exports = router;
